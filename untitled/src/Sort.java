62f04d080cff1abe5b58ca713618490d0840d5f2
huangshaofan <2595476383@qq.com>
2019-03-31 17:11:42 +0800

    第一天

---
+++ untitled/src/Sort.java
@@ -0,0 +1,11 @@
+public class Sort {
+    public static void main(String[] args) {
+        int[] a={2,7,9,5,1,6};
+    }
+
+    public static void sort(int[] a) {
+        for (int i = 1; i <a.length ; i++) {
+
+        }
+    }
+}
